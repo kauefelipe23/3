f06eebd04ec1c53171cf54f2afb1976f4892f274
WaldinhoGamer <waldaotto@gmail.com>
2025-05-12 22:02:50 -0300

    Add files via upload
    BASE PRO AVIAO

---
+++ base-aviao.php
@@ -0,0 +1,26 @@
+<?php
+
+$numlinha = 11;
+$numcoluna = 7;
+
+$assento[0][0] = null;
+$assento[1][0] = 1;
+$assento[2][0] = 2;
+$assento[3][0] = 3;
+$assento[4][0] = 4;
+$assento[5][0] = 5;
+$assento[6][0] = 6;
+$assento[7][0] = 7;
+$assento[8][0] = 8;
+$assento[9][0] = 9;
+$assento[10][0] = 10;
+
+$assento[0][1] = "A";
+$assento[0][2] = "B";
+$assento[0][3] = "C";
+$assento[0][4] = "D";
+$assento[0][5] = "E";
+$assento[0][6] = "F";
+
+var_dump($assento);
+?>
